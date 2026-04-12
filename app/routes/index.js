01338767a1feff14ad87c6c17c65aaf6e44c6ca3
GansonKari-FS <kDganson@student.fullsail.edu>
2026-04-11 22:01:03 -0500

    dont remember

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,8 +1,8 @@
 const express = require(`express`);
 const router = express.Router();
 
-router.get(`/`, (req, res) => {
-  res.status(200).json({ message: `From the API` });
+router.get("/", (req, res) => {
+  res.status(200).json({ message: "From the API" });
 });
 
 router.post("/", (req, res) => {
